bf6c5abaa4e283d2dcac55aefca36e86ae580329
Var7600 <77991946+Var7600@users.noreply.github.com>
2024-12-23 18:49:42 -0500

    feat: add Component TFlipFlop and refactor DisplayLibrary class
    Extract constant library component in DisplayLibrary to a new class
    Library.
    
    Extract some method in DisplayLibrary to some static util method in
    class ComponentUtil.

---
+++ src/main/java/vhdlgenerator/component/Library.java
@@ -0,0 +1,83 @@
+/**
+ * @file Library.java
+ *
+ * @author DOUDOU DIAWARA @see
+ * <a href="https://github.com/Var7600/VHDL-GENERATOR">Github Page</a>
+ *
+ * @version 0.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of “Commons Clause” License Condition v1.0 but not for Commercial
+ * use
+ *
+ */
+package vhdlgenerator.component;
+
+/**
+ * this class hold library available
+ *
+ * @author DOUDOU DIAWARA
+ */
+public final class Library
+{
+
+	private Library()
+	{
+	} // prevent instantiation
+
+	//
+	// LIBRARY COMPONENT
+	//
+
+	/**
+	 * ENCODER
+	 */
+	public static final String ENCODER = "Encoder";
+	/**
+	 * DECODER
+	 */
+	public static final String DECODER = "Decoder";
+	/**
+	 * PRIORITY ENCODER
+	 */
+	public static final String PRIORITY_ENCODER = "Priority Encoder";
+	/**
+	 * MULTIPLEXER
+	 */
+	public static final String MULTIPLEXER = "Multiplexer";
+	/**
+	 * DEMULTIPLEXER
+	 */
+	public static final String DEMULTIPLEXER = "Demultiplexer";
+	/**
+	 * SEVEN SEGEMENT DISPLAY
+	 */
+	public static final String SEVEN_SEGMENT = "7-Segment-Display";
+	/**
+	 * ADDER
+	 */
+	public static final String ADDER = "Adder";
+	/**
+	 * SOUSTRACTOR
+	 */
+	public static final String SUBTRACTOR = "Subtractor";
+	/**
+	 * COMPARATOR
+	 */
+	public static final String COMPARATOR = "Comparator";
+	/**
+	 * D-FLIP-FLOP
+	 */
+	public static final String DFILPFLOP = "D-Flip-Flop";
+	/**
+	 * JK-FLIP-FLOP
+	 */
+	public static final String JKFLIPFLOP = "JK-Flip-Flop";
+	/**
+	 * T-FLIP-FLOP
+	 */
+	public static final String TFLIPFLOP = "T-Flip-Flop";
+
+}
